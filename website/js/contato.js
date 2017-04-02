1b40ffaa1065d00841d81612a9a288609d1af00e
moliveira <moliveira@aconex.com>
2017-04-03 09:57:19 +1000

    Mensagem de confirmacao ao enviar email

--- website/js/contato.js
+++ website/js/contato.js
@@ -1,6 +1,5 @@
 function sendEnquire(fromModal){
     var message = "Requisicao do Site \n\n" +
-                // "Tipo: " + $("#tipoSolicitacao").val() + "\n\n" +
                 "Nome Contato: " + $("#nomeContato").val() + "\n\n" +
                 "Empresa: " + $("#empresaContato").val() + "\n\n" +
                 "Telefone Contato: " + $("#telefoneContato").val() + "\n\n" +
@@ -13,29 +12,25 @@ function sendEnquire(fromModal){
       message: message
     };
 
+    var cleanForm = function(){
+        $("form input").val('');
+        $("form textarea").val('');
+        $("#sendMessage").prop('disabled', false);
+    };
+
+    $("#sendMessage").prop('disabled', true);
+
     $.ajax({
         url: "https://formspree.io/vendas@purewaterefluentes.com.br",
         method: "POST",
         data: mail,
         dataType: "json",
-        success: function(data, textStatus, jqXHR){
-          $(".alert-success").show();
-          
-          fromModal && $("#contatoModal").modal("hide");
+        success: function(){
+            $("#confirmationModal").modal("show");
+            $("#confirmationModal").on('hidden.bs.modal', cleanForm);
         },
-        error: function(jqXHR, textStatus, errorThrown){
+        error: function(){
           $(".alert-danger").show()
         }
     });  
 }
-
-
-// $('#contatoModal').on('show.bs.modal', function (event) {
-//   var button = $(event.relatedTarget) // Button that triggered the modal
-//   var recipient = button.data('whatever') // Extract info from data-* attributes
-//   // If necessary, you could initiate an AJAX request here (and then do the updating in a callback).
-//   // Update the modal's content. We'll use jQuery here, but you could use a data binding library or other methods instead.
-//   var modal = $(this)
-//   modal.find('.modal-title').text('New message to ' + recipient)
-//   modal.find('.modal-body input').val(recipient)
-// })
