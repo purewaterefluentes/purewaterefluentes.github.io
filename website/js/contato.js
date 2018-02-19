25114b6db2666baa8b9bfb3aa510934e26a8b707
Marcelo Garcia <marcelo.mgo@gmail.com>
2018-02-19 12:57:23 +1100

    Envio de email pela locaweb

--- website/js/contato.js
+++ website/js/contato.js
@@ -7,6 +7,46 @@ function sendEnquire(fromModal){
                 "Origem do contato: " + $.urlParam("s") + "\n\n" +
                 "Mensagem: " + $("#mensagem").val();
 
+    var postData = {
+      nome: $("#nomeContato").val(),
+      empresa: $("#empresaContato").val(),
+      telefone: $("#telefoneContato").val(),
+      email: $("#emailContato").val(),
+      message: message
+    };
+
+    var cleanForm = function(){
+        $("form input").val('');
+        $("form textarea").val('');
+        $("#sendMessage").prop('disabled', false);
+    };
+
+    $("#sendMessage").prop('disabled', true);
+
+    $.ajax({
+        url: "http://purewaterefluentes.tempsite.ws/sendEmail.php",
+        method: "POST",
+        data: postData,
+        dataType: "json",
+        success: function(){
+            $("#confirmationModal").modal("show");
+            $("#confirmationModal").on('hidden.bs.modal', cleanForm);
+        },
+        error: function(){
+          $(".alert-danger").show()
+        }
+    });  
+}
+
+function sendEnquireFormspree(fromModal){
+    var message = "Requisicao do Site \n\n" +
+                "Nome Contato: " + $("#nomeContato").val() + "\n\n" +
+                "Empresa: " + $("#empresaContato").val() + "\n\n" +
+                "Telefone Contato: " + $("#telefoneContato").val() + "\n\n" +
+                "Email Contato: " + $("#emailContato").val() + "\n\n" +
+                "Origem do contato: " + $.urlParam("s") + "\n\n" +
+                "Mensagem: " + $("#mensagem").val();
+
     var mail = {
       _replyto: $("#emailContato").val(),
       _subject: "Requisicao do Site - " + $("#nomeContato").val(),
@@ -35,6 +75,7 @@ function sendEnquire(fromModal){
         }
     });  
 }
+
 
 $.urlParam = function(name){
     var results = new RegExp('[\?&]' + name + '=([^&#]*)').exec(window.location.href);
